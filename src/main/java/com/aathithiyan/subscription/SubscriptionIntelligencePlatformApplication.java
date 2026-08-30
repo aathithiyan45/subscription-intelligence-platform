7eb2babfbe530f57891f9963346f9c949e7cef9b
aathithiyan45 <aathimi07012006@gmail.com>
2026-08-30 19:46:40 +0530

    chore: add Spring Boot project setup

---
+++ src/main/java/com/aathithiyan/subscription/SubscriptionIntelligencePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.aathithiyan.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionIntelligencePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionIntelligencePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aathithiyan/subscription/SubscriptionIntelligencePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aathithiyan.subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionIntelligencePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
